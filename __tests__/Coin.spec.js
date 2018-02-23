64556f12b20d4cd77b7829e6dda7c129deaf64fd
Lindsey <lindsey@users-MacBook-Pro.local>
2018-02-23 11:13:57 -0800

    created coin class with constructor and tests

--- __tests__/Coin.spec.js
+++ __tests__/Coin.spec.js
@@ -1,19 +1,21 @@
 const Coin = require("../src/Coin");
 
 describe("Coin tests", () => {
-  let Coin;
+  let coin;
+  let coinWithValue;
   beforeEach(() => {
     coin = new Coin();
+    coinWithValue = new Coin(0.05);
   });
   describe("Successful Coin Transactions", () => {
-    test("Create ", () => {
-      expect().toEqual();
+    test("Create a coin with a value of 0.05", () => {
+      expect(coinWithValue).toEqual({ value: 0.05 });
     });
   });
 
   describe("Edge Case Coin Transactions", () => {
-    test("Create", () => {
-      expect().toEqual();
+    test("Create a coin with no value given", () => {
+      expect(coin).toEqual({ value: 0.01 });
     });
   });
 });
---
+++ src/Coin.js
@@ -0,0 +1,26 @@
+const DEFAULT_COIN_VALUE = 0.01;
+module.exports = class Coin {
+  // Constructor
+  // @params a int or double value indicating how much a coin is worth
+  // @returns a coin with the given value or a default of 0.01 if no value given
+  constructor(value) {
+    if (isValidValue(value)) this.value = value;
+    else this.value = DEFAULT_COIN_VALUE;
+  }
+
+  // @params: none
+  // @ returns: the value of the coin
+  getValue() {
+    return this.value;
+  }
+};
+
+// @params: value to validate
+// @returns: true if this value is valid and false otherwise
+function isValidValue(value) {
+  // a coin is valid if:
+  // - the value exists
+  // - it is an int or double
+  // - it is more than 0
+  return !(!value || Number.isNaN(value) || value < 0);
+}
