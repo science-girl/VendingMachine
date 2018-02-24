244a1012573dd5b54e9d59e855c3876efa1862b2
Lindsey <lindsey@users-MacBook-Pro.local>
2018-02-23 17:29:36 -0800

    refactored CoinBank to accept an array of coin denominations

--- __tests__/CoinBank.spec.js
+++ __tests__/CoinBank.spec.js
@@ -1,4 +1,19 @@
 const CoinBank = require("../src/CoinBank");
+// Constants
+const TWO_DOLLAR_ACCESSOR = "TwoDollars";
+const ONE_DOLLAR_ACCESSOR = "OneDollar";
+const QUARTER_ACCESSOR = "Quarter";
+const DIME_ACCESSOR = "Dime";
+const NICKEL_ACCESSOR = "Nickel";
+
+const DENOMINATIONS = [
+  { name: TWO_DOLLAR_ACCESSOR, value: 2.0 },
+  { name: ONE_DOLLAR_ACCESSOR, value: 1.0 },
+  { name: QUARTER_ACCESSOR, value: 0.25 },
+  { name: DIME_ACCESSOR, value: 0.1 },
+  { name: NICKEL_ACCESSOR, value: 0.05 }
+];
+
 const DEFAULT_BANK = {
   bank: {
     Dime: {
@@ -28,7 +43,7 @@ describe("CoinBank tests", () => {
   let bank;
 
   beforeEach(() => {
-    bank = new CoinBank();
+    bank = new CoinBank(DENOMINATIONS);
   });
   describe("Successful CoinBank Transactions", () => {
     test("Create an empty bank", () => {
@@ -41,35 +56,53 @@ describe("CoinBank tests", () => {
     test("Get bank balance with funds available", () => {
       expect().toEqual();
     });
+    test("Get coin balance with funds available", () => {
+      expect(bank.depositCoin(TWO_DOLLAR_ACCESSOR, 4)).toEqual(true);
+      expect(bank.getCoinBalance(TWO_DOLLAR_ACCESSOR)).toEqual(8.0);
+    });
+    test("Get number of coins with funds available", () => {
+      expect(bank.depositCoin(TWO_DOLLAR_ACCESSOR, 4)).toEqual(true);
+      expect(bank.getCoinNumber(TWO_DOLLAR_ACCESSOR)).toEqual(4);
+    });
     test("Deposit 5 two dollar coins", () => {
-      expect().toEqual();
+      expect(bank.depositCoin(TWO_DOLLAR_ACCESSOR, 5)).toEqual(true);
     });
     test("Deposit 5 dollar coins", () => {
-      expect().toEqual();
+      expect(bank.depositCoin(ONE_DOLLAR_ACCESSOR, 5)).toEqual(true);
     });
     test("Deposit 5 quarters", () => {
-      expect().toEqual();
+      expect(bank.depositCoin(QUARTER_ACCESSOR, 5)).toEqual(true);
     });
     test("Deposit 5 dimes", () => {
-      expect().toEqual();
+      expect(bank.depositCoin(DIME_ACCESSOR, 5)).toEqual(true);
     });
     test("Deposit 5 nickels", () => {
-      expect().toEqual();
+      expect(bank.depositCoin(NICKEL_ACCESSOR, 5)).toEqual(true);
     });
     test("Withdraw 5 two dollar coins", () => {
-      expect().toEqual();
+      expect(bank.depositCoin(TWO_DOLLAR_ACCESSOR, 6)).toEqual(true);
+      expect(bank.withdrawCoin(TWO_DOLLAR_ACCESSOR, 5)).toEqual(true);
+      expect(bank.getCoinNumber(TWO_DOLLAR_ACCESSOR)).toEqual(1);
     });
     test("Withdraw 5 dollar coins", () => {
-      expect().toEqual();
+      expect(bank.depositCoin(ONE_DOLLAR_ACCESSOR, 6)).toEqual(true);
+      expect(bank.withdrawCoin(ONE_DOLLAR_ACCESSOR, 5)).toEqual(true);
+      expect(bank.getCoinNumber(ONE_DOLLAR_ACCESSOR)).toEqual(1);
     });
     test("Withdraw 5 quarters", () => {
-      expect().toEqual();
+      expect(bank.depositCoin(QUARTER_ACCESSOR, 6)).toEqual(true);
+      expect(bank.withdrawCoin(QUARTER_ACCESSOR, 5)).toEqual(true);
+      expect(bank.getCoinNumber(QUARTER_ACCESSOR)).toEqual(1);
     });
     test("Withdraw 5 dimes", () => {
-      expect().toEqual();
+      expect(bank.depositCoin(DIME_ACCESSOR, 6)).toEqual(true);
+      expect(bank.withdrawCoin(DIME_ACCESSOR, 5)).toEqual(true);
+      expect(bank.getCoinNumber(DIME_ACCESSOR)).toEqual(1);
     });
     test("Withdraw 5 nickels", () => {
-      expect().toEqual();
+      expect(bank.depositCoin(NICKEL_ACCESSOR, 6)).toEqual(true);
+      expect(bank.withdrawCoin(NICKEL_ACCESSOR, 5)).toEqual(true);
+      expect(bank.getCoinNumber(NICKEL_ACCESSOR)).toEqual(1);
     });
   });
 
@@ -78,28 +111,37 @@ describe("CoinBank tests", () => {
       expect().toEqual();
     });
     test("Deposit a coin denomination that doesn't exist in the CoinBank", () => {
-      expect().toEqual();
+      expect(bank.depositCoin("NotACoin", 5)).toEqual(false);
     });
     test("Deposit a negative amount", () => {
-      expect().toEqual();
+      expect(bank.depositCoin(TWO_DOLLAR_ACCESSOR, -5)).toEqual(false);
+    });
+    test("Deposit two dollar coins with no argument", () => {
+      expect(bank.depositCoin(TWO_DOLLAR_ACCESSOR)).toEqual(false);
+    });
+    test("Get coin balance with no funds available", () => {
+      expect(bank.getCoinBalance(TWO_DOLLAR_ACCESSOR)).toEqual(0);
+    });
+    test("Get coin balance for coin that does not exist", () => {
+      expect(bank.getCoinBalance("NotACoin")).toEqual(false);
     });
     test("Withdraw a coin denomination that doesn't exist in the CoinBank", () => {
-      expect().toEqual();
+      expect(bank.withdrawCoin("NotACoin", 5)).toEqual(false);
     });
     test("Withdraw 5 two dollar coins with no funds available", () => {
-      expect().toEqual();
+      expect(bank.withdrawCoin(TWO_DOLLAR_ACCESSOR, 5)).toEqual(false);
     });
     test("Withdraw 5 dollar coins with no funds available", () => {
-      expect().toEqual();
+      expect(bank.withdrawCoin(ONE_DOLLAR_ACCESSOR, 5)).toEqual(false);
     });
     test("Withdraw 5 quarters with no funds available", () => {
-      expect().toEqual();
+      expect(bank.withdrawCoin(QUARTER_ACCESSOR, 5)).toEqual(false);
     });
     test("Withdraw 5 dimes with no funds available", () => {
-      expect().toEqual();
+      expect(bank.withdrawCoin(DIME_ACCESSOR, 5)).toEqual(false);
     });
     test("Withdraw 5 nickels with no funds available", () => {
-      expect().toEqual();
+      expect(bank.withdrawCoin(NICKEL_ACCESSOR, 5)).toEqual(false);
     });
   });
 });
--- src/CoinBank.js
+++ src/CoinBank.js
@@ -1,27 +1,23 @@
 const Coin = require("./Coin");
 
 // Constants
-const TWO_DOLLAR_ACCESSOR = "TwoDollars";
-const ONE_DOLLAR_ACCESSOR = "OneDollar";
-const QUARTER_ACCESSOR = "Quarter";
-const DIME_ACCESSOR = "Dime";
-const NICKEL_ACCESSOR = "Nickel";
 const DEFAULT_COIN_QUANTITY = 0;
 
 module.exports = class CoinBank {
   // Constructor
   // @params: none
-  // @returns: a bank object consisting of a default CoinBank:
-  // - TwoDollars($2.00), OneDollar($1.00), Quarters (0.25), Dimes (0.10), Nickels (0.05)
+  // @returns: a bank object consisting of a CoinBank:
   // - {bank: {<coin_name>: {coin: <Coin>}, ...}
-  // TODO: accept an array of different Coin denominations which override the default config
-  constructor() {
+  constructor(array) {
     this.bank = {};
-    this.bank[TWO_DOLLAR_ACCESSOR] = new Coin(2.0, DEFAULT_COIN_QUANTITY);
-    this.bank[ONE_DOLLAR_ACCESSOR] = new Coin(1.0, DEFAULT_COIN_QUANTITY);
-    this.bank[QUARTER_ACCESSOR] = new Coin(0.25, DEFAULT_COIN_QUANTITY);
-    this.bank[DIME_ACCESSOR] = new Coin(0.1, DEFAULT_COIN_QUANTITY);
-    this.bank[NICKEL_ACCESSOR] = new Coin(0.05, DEFAULT_COIN_QUANTITY);
+    if (array) {
+      array.forEach(denomination => {
+        this.bank[denomination.name] = new Coin(
+          denomination.value,
+          DEFAULT_COIN_QUANTITY
+        );
+      });
+    }
   }
 
   // @params: none
@@ -32,25 +28,48 @@ module.exports = class CoinBank {
 
   // @params: string coinName to update by int numberOfCoins
   // @returns: true if the deposit was successful and false otherwise
-  depositCoin(coinName, numberOfCoins) {}
+  depositCoin(coinName, numberOfCoins) {
+    if (this.isCoinInBank(coinName)) {
+      return this.bank[coinName].increaseQuantity(numberOfCoins);
+    }
+    return false;
+  }
 
   // @params: string coinName to withdraw and the int numberOfCoins to withdraw
   // @returns: true if the withdrawal was successful and false otherwise
-  withdrawCoin(coinName, numberOfCoins) {}
+  withdrawCoin(coinName, numberOfCoins) {
+    if (this.isCoinInBank(coinName)) {
+      return this.bank[coinName].decreaseQuantity(numberOfCoins);
+    }
+    return false;
+  }
 
   // @params: string coinName
-  // @returns: the balance of the given string coinName
-  getCoinBalance(coinName) {}
+  // @returns: the balance of the given string coinName, otherwise false
+  getCoinBalance(coinName) {
+    if (this.isCoinInBank(coinName)) {
+      return this.bank[coinName].getQuantity() * this.bank[coinName].getValue();
+    }
+    return false;
+  }
 
   // @params: string coinName
-  // @returns: the int numberOfCoins available for the given coinName
-  getCoinNumber(coinName) {}
+  // @returns: the int numberOfCoins available for the given coinName,
+  // otherwise returns false
+  getCoinNumber(coinName) {
+    if (this.isCoinInBank(coinName)) {
+      return this.bank[coinName].getQuantity();
+    }
+    return false;
+  }
 
   // @params: none
-  // @returns: the total balance of the coin banke
+  // @returns: the total balance of the coin bank
   getBalance() {}
-};
 
-// @params:
-// @returns:
-function isValidCoinName(coinName) {}
+  // @params string coinName to check exists in bank
+  // @returns true if the coin exists and false otherwise
+  isCoinInBank(coinName) {
+    return coinName in this.bank;
+  }
+};
