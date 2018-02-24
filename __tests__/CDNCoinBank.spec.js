43352f5699b249807860d4b7bc1517c35f6782c1
Lindsey <lindsey@users-MacBook-Pro.local>
2018-02-23 20:49:12 -0800

    added implementation and testing for increasing and decreasing denominations

--- __tests__/CDNCoinBank.spec.js
+++ __tests__/CDNCoinBank.spec.js
@@ -10,14 +10,107 @@ describe("Canadian CoinBank tests", () => {
     bank = new CDNCoinBank();
   });
   describe("Successful Canadian CoinBank Transactions", () => {
-    test("Create an empty bank", () => {
-      expect().toEqual();
+    test("Increase Two Dollar Coins", () => {
+      expect(bank.increaseTwoDollarCoins(4)).toEqual(true);
+    });
+    test("Increase One Dollar Coins", () => {
+      expect(bank.increaseOneDollarCoins(4)).toEqual(true);
+    });
+    test("Increase Quarters", () => {
+      expect(bank.increaseQuarters(4)).toEqual(true);
+    });
+    test("Increase Dimes", () => {
+      expect(bank.increaseDimes(4)).toEqual(true);
+    });
+    test("Increase Nickels", () => {
+      expect(bank.increaseNickels(4)).toEqual(true);
+    });
+    test("Decrease Two Dollar Coins", () => {
+      expect(bank.increaseTwoDollarCoins(4)).toEqual(true);
+      expect(bank.decreaseTwoDollarCoins(3)).toEqual(true);
+    });
+    test("Decrease One Dollar Coins", () => {
+      expect(bank.increaseOneDollarCoins(4)).toEqual(true);
+      expect(bank.decreaseOneDollarCoins(3)).toEqual(true);
+    });
+    test("Decrease Quarters", () => {
+      expect(bank.increaseQuarters(4)).toEqual(true);
+      expect(bank.decreaseQuarters(3)).toEqual(true);
+    });
+    test("Decrease Dimes", () => {
+      expect(bank.increaseDimes(4)).toEqual(true);
+      expect(bank.decreaseDimes(3)).toEqual(true);
+    });
+    test("Decrease Nickels", () => {
+      expect(bank.increaseNickels(4)).toEqual(true);
+      expect(bank.decreaseNickels(3)).toEqual(true);
+    });
+    test("Get number of two dollar coins", () => {
+      expect(bank.increaseTwoDollarCoins(4)).toEqual(true);
+      expect(bank.getNumberOfTwoDollarCoins()).toEqual(4);
+    });
+    test("Get number of one dollar coins", () => {
+      expect(bank.increaseOneDollarCoins(4)).toEqual(true);
+      expect(bank.getNumberOfOneDollarCoins()).toEqual(4);
+    });
+    test("Get number of quarters", () => {
+      expect(bank.increaseQuarters(4)).toEqual(true);
+      expect(bank.getNumberOfQuarterCoins()).toEqual(4);
+    });
+    test("Get number of dimes", () => {
+      expect(bank.increaseDimes(4)).toEqual(true);
+      expect(bank.getNumberOfDimeCoins()).toEqual(4);
+    });
+    test("Get number of nickels", () => {
+      expect(bank.increaseNickels(4)).toEqual(true);
+      expect(bank.getNumberOfNickelCoins()).toEqual(4);
+    });
+
+    test("Get balance of two dollar coins", () => {
+      expect(bank.increaseTwoDollarCoins(4)).toEqual(true);
+      expect(bank.getTwoDollarBalance()).toEqual(8);
+    });
+    test("Get balance of one dollar coins", () => {
+      expect(bank.increaseOneDollarCoins(4)).toEqual(true);
+      expect(bank.getOneDollarBalance()).toEqual(4);
+    });
+    test("Get balance of quarters", () => {
+      expect(bank.increaseQuarters(4)).toEqual(true);
+      expect(bank.getQuarterBalance()).toEqual(1);
+    });
+    test("Get balance of dimes", () => {
+      expect(bank.increaseDimes(4)).toEqual(true);
+      expect(bank.getDimeBalance()).toEqual(0.4);
+    });
+    test("Get balance of nickels", () => {
+      expect(bank.increaseNickels(4)).toEqual(true);
+      expect(bank.getNickelBalance()).toEqual(0.2);
     });
   });
 
   describe("Edge Case Canadian CoinBank Transactions", () => {
     test("Get bank balance with no funds available", () => {
       expect().toEqual();
+    });
+    test("Decrease Two Dollar Coins", () => {
+      expect(bank.increaseTwoDollarCoins(4)).toEqual(true);
+      expect(bank.decreaseTwoDollarCoins(5)).toEqual(false);
+    });
+    test("Decrease One Dollar Coins", () => {
+      expect(bank.increaseOneDollarCoins(4)).toEqual(true);
+      expect(bank.decreaseOneDollarCoins(5)).toEqual(false);
+    });
+    test("Decrease Quarters", () => {
+      expect(bank.increaseQuarters(4)).toEqual(true);
+      expect(bank.decreaseQuarters(5)).toEqual(false);
+    });
+    test("Decrease Dimes", () => {
+      expect(bank.increaseDimes(4)).toEqual(true);
+      expect(bank.decreaseDimes(5)).toEqual(false);
+    });
+    test("Decrease Nickels", () => {
+      expect(bank.increaseNickels(4)).toEqual(true);
+      expect(bank.decreaseNickels(5)).toEqual(false);
     });
   });
 });
--- src/CDNCoinBank.js
+++ src/CDNCoinBank.js
@@ -17,36 +17,128 @@ module.exports = class CDNCoinBank {
   // - {bank: {<coin_name>: {coin: <Coin>}, ...}
   constructor() {
     this.bank = new CoinBank(DENOMINATIONS);
-    console.log(this.bank);
+    //console.log(this.bank);
   }
 
   // @params: none
   // @returns: int numberOfTwoDollar coins
   getNumberOfTwoDollarCoins() {
-    return this.bank.getQuantity(DENOMINATIONS[0].name);
+    return this.bank.getCoinNumber(DENOMINATIONS[0].name);
   }
 
   // @params: none
   // @returns: int numberOfOneDollar coins
   getNumberOfOneDollarCoins() {
-    return this.bank.getQuantity(DENOMINATIONS[1].name);
+    return this.bank.getCoinNumber(DENOMINATIONS[1].name);
   }
 
   // @params: none
   // @returns: int numberOfQuarter coins
   getNumberOfQuarterCoins() {
-    return this.bank.getQuantity(DENOMINATIONS[2].name);
+    return this.bank.getCoinNumber(DENOMINATIONS[2].name);
   }
 
   // @params: none
   // @returns: int numberOfDime coins
   getNumberOfDimeCoins() {
-    return this.bank.getQuantity(DENOMINATIONS[3].name);
+    return this.bank.getCoinNumber(DENOMINATIONS[3].name);
   }
 
   // @params: none
   // @returns: int numberOfNickel coins
   getNumberOfNickelCoins() {
-    return this.bank.getQuantity(DENOMINATIONS[4].name);
+    return this.bank.getCoinNumber(DENOMINATIONS[4].name);
+  }
+
+  // @params: int numberOfTwoDollarCoins
+  // @returns: true if increased and false otherwise
+  increaseTwoDollarCoins(numberOfTwoDollarCoins) {
+    return this.bank.depositCoin(DENOMINATIONS[0].name, numberOfTwoDollarCoins);
+  }
+  // @params: int numberOfOneDollarCoins
+  // @returns: true if increased and false otherwise
+  increaseOneDollarCoins(numberOfOneDollarCoins) {
+    return this.bank.depositCoin(DENOMINATIONS[1].name, numberOfOneDollarCoins);
+  }
+  // @params: int numberOfQuarters
+  // @returns: true if increased and false otherwise
+  increaseQuarters(numberOfQuarters) {
+    return this.bank.depositCoin(DENOMINATIONS[2].name, numberOfQuarters);
+  }
+  // @params: int numberOfDimes
+  // @returns: true if increased and false otherwise
+  increaseDimes(numberOfDimes) {
+    return this.bank.depositCoin(DENOMINATIONS[3].name, numberOfDimes);
+  }
+  // @params: int numberOfNickels
+  // @returns: true if increased and false otherwise
+  increaseNickels(numberOfNickels) {
+    return this.bank.depositCoin(DENOMINATIONS[4].name, numberOfNickels);
+  }
+  // @params: int numberOfTwoDollarCoins
+  // @returns: true if decreased and false otherwise
+  decreaseTwoDollarCoins(numberOfTwoDollarCoins) {
+    return this.bank.withdrawCoin(
+      DENOMINATIONS[0].name,
+      numberOfTwoDollarCoins
+    );
+  }
+  // @params: int numberOfOneDollarCoins
+  // @returns: true if decreased and false otherwise
+  decreaseOneDollarCoins(numberOfOneDollarCoins) {
+    return this.bank.withdrawCoin(
+      DENOMINATIONS[1].name,
+      numberOfOneDollarCoins
+    );
+  }
+  // @params: int numberOfQuarters
+  // @returns: true if decreased and false otherwise
+  decreaseQuarters(numberOfQuarters) {
+    return this.bank.withdrawCoin(DENOMINATIONS[2].name, numberOfQuarters);
+  }
+  // @params: int numberOfDimes
+  // @returns: true if decreased and false otherwise
+  decreaseDimes(numberOfDimes) {
+    return this.bank.withdrawCoin(DENOMINATIONS[3].name, numberOfDimes);
+  }
+  // @params: int numberOfNickels
+  // @returns: true if decreased and false otherwise
+  decreaseNickels(numberOfNickels) {
+    return this.bank.withdrawCoin(DENOMINATIONS[4].name, numberOfNickels);
+  }
+
+  // @params: none
+  // @returns: the monetary two dollar balance
+  getTwoDollarBalance() {
+    return this.bank.getCoinBalance(DENOMINATIONS[0].name);
+  }
+
+  // @params: none
+  // @returns: the monetary dollar balance
+  getOneDollarBalance() {
+    return this.bank.getCoinBalance(DENOMINATIONS[1].name);
+  }
+
+  // @params: none
+  // @returns: the monetary quarter balance
+  getQuarterBalance() {
+    return this.bank.getCoinBalance(DENOMINATIONS[2].name);
+  }
+  // @params: none
+  // @returns: the monetary dime balance
+  getDimeBalance() {
+    return this.bank.getCoinBalance(DENOMINATIONS[3].name);
+  }
+
+  // @params: none
+  // @returns: the monetary nickel balance
+  getNickelBalance() {
+    return this.bank.getCoinBalance(DENOMINATIONS[4].name);
+  }
+
+  // @params: none
+  // @returns: the monetary balance of the bank
+  getBalance() {
+    return this.bank.getBalance();
   }
 };
