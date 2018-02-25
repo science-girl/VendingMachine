1d8ead676595f47e89841620b387f968e26f9955
Lindsey <lindsey@users-MBP.hitronhub.home>
2018-02-24 18:59:27 -0800

    refactored test data

--- __tests__/Coin.spec.js
+++ __tests__/Coin.spec.js
@@ -1,7 +1,8 @@
-const Coin = require("../src/Coin");
+const Coin = require('../src/Coin');
+
 const DEFAULT_COIN_VALUE = { quantity: 0, value: 0.05 };
 
-describe("Coin tests", () => {
+describe('Coin tests', () => {
   let coin;
   let coinWithValue;
   let coinWithQuantity;
@@ -10,74 +11,74 @@ describe("Coin tests", () => {
     coinWithValue = new Coin(0.1);
     coinWithQuantity = new Coin(0.1, 2);
   });
-  describe("Successful Coin Transactions", () => {
-    test("Create a coin with a value of 0.10 and no quantity given", () => {
+  describe('Successful Coin Transactions', () => {
+    test('Create a coin with a value of 0.10 and no quantity given', () => {
       expect(coinWithValue).toEqual({ value: 0.1, quantity: 0 });
     });
-    test("Create a coin with a value of 0.10 quantity given", () => {
+    test('Create a coin with a value of 0.10 quantity given', () => {
       expect(coinWithQuantity).toEqual({ value: 0.1, quantity: 2 });
     });
-    test("Get a coin with quantity given", () => {
+    test('Get a coin with quantity given', () => {
       expect(coinWithQuantity.getQuantity()).toEqual(2);
     });
-    test("Get a coin value", () => {
+    test('Get a coin value', () => {
       expect(coinWithValue.getValue()).toEqual(0.1);
     });
-    test("Set a coin value to 0.15", () => {
+    test('Set a coin value to 0.15', () => {
       expect(coin.setValue(0.15)).toEqual(true);
       expect(coin.getValue()).toEqual(0.15);
     });
-    test("Set a coin quantity to 15", () => {
+    test('Set a coin quantity to 15', () => {
       expect(coin.setQuantity(15)).toEqual(true);
       expect(coin.getQuantity()).toEqual(15);
     });
-    test("Increase a non-zero coin quantity by 5", () => {
+    test('Increase a non-zero coin quantity by 5', () => {
       expect(coin.setQuantity(15)).toEqual(true);
       expect(coin.increaseQuantity(5)).toEqual(true);
       expect(coin.getQuantity()).toEqual(20);
     });
-    test("Decrease a non-zero coin quantity by 5", () => {
+    test('Decrease a non-zero coin quantity by 5', () => {
       expect(coin.setQuantity(15)).toEqual(true);
       expect(coin.decreaseQuantity(5)).toEqual(true);
       expect(coin.getQuantity()).toEqual(10);
     });
   });
 
-  describe("Edge Case Coin Transactions", () => {
-    test("Create a coin with no value given", () => {
+  describe('Edge Case Coin Transactions', () => {
+    test('Create a coin with no value given', () => {
       expect(coin).toEqual(DEFAULT_COIN_VALUE);
     });
-    test("Create a coin with a negative value", () => {
+    test('Create a coin with a negative value', () => {
       const negCoin = new Coin(-0.3);
       expect(negCoin).toEqual(DEFAULT_COIN_VALUE);
     });
-    test("Create a coin with more than 2 floating point values", () => {
+    test('Create a coin with more than 2 floating point values', () => {
       const floatCoin = new Coin(0.345);
       expect(floatCoin).toEqual({ value: 0.35, quantity: 0 });
     });
-    test("Create a coin with a value greater than 2", () => {
+    test('Create a coin with a value greater than 2', () => {
       const dollar = new Coin(Coin.MAX_COIN_VALUE + 1);
       expect(dollar).toEqual(DEFAULT_COIN_VALUE);
     });
-    test("Create a coin with a non-numeric value", () => {
-      const nonNumericCoin = new Coin("a");
+    test('Create a coin with a non-numeric value', () => {
+      const nonNumericCoin = new Coin('a');
       expect(nonNumericCoin).toEqual(DEFAULT_COIN_VALUE);
     });
-    test("Get a coin value with no set value", () => {
+    test('Get a coin value with no set value', () => {
       expect(coin.getValue()).toEqual(0.05);
     });
-    test("Set a coin value to a negative value", () => {
+    test('Set a coin value to a negative value', () => {
       expect(coin.setValue(-0.15)).toEqual(false);
       expect(coin.getValue()).toEqual(0.05);
     });
-    test("Decrease a coin with 0 quantity by 5", () => {
+    test('Decrease a coin with 0 quantity by 5', () => {
       expect(coin.decreaseQuantity(5)).toEqual(false);
       expect(coin.getQuantity()).toEqual(0);
     });
-    test("Get a coin quantity with no set value", () => {
+    test('Get a coin quantity with no set value', () => {
       expect(coin.getQuantity()).toEqual(0);
     });
-    test("Set a coin quantity to a negative value", () => {
+    test('Set a coin quantity to a negative value', () => {
       expect(coin.setQuantity(-0.15)).toEqual(false);
       expect(coin.getQuantity()).toEqual(0);
     });
--- __tests__/CoinBank.spec.js
+++ __tests__/CoinBank.spec.js
@@ -1,150 +1,150 @@
-const CoinBank = require("../src/CoinBank");
+const CoinBank = require('../src/CoinBank');
 // Constants
-const TWO_DOLLAR_ACCESSOR = "TwoDollars";
-const ONE_DOLLAR_ACCESSOR = "OneDollar";
-const QUARTER_ACCESSOR = "Quarter";
-const DIME_ACCESSOR = "Dime";
-const NICKEL_ACCESSOR = "Nickel";
+const TWO_DOLLAR_ACCESSOR = 'TwoDollars';
+const ONE_DOLLAR_ACCESSOR = 'OneDollar';
+const QUARTER_ACCESSOR = 'Quarter';
+const DIME_ACCESSOR = 'Dime';
+const NICKEL_ACCESSOR = 'Nickel';
 
 const DENOMINATIONS = [
   { name: TWO_DOLLAR_ACCESSOR, value: 2.0 },
   { name: ONE_DOLLAR_ACCESSOR, value: 1.0 },
   { name: QUARTER_ACCESSOR, value: 0.25 },
   { name: DIME_ACCESSOR, value: 0.1 },
-  { name: NICKEL_ACCESSOR, value: 0.05 }
+  { name: NICKEL_ACCESSOR, value: 0.05 },
 ];
 
 const DEFAULT_BANK = {
   bank: {
     Dime: {
       quantity: 0,
-      value: 0.1
+      value: 0.1,
     },
     Nickel: {
       quantity: 0,
-      value: 0.05
+      value: 0.05,
     },
     OneDollar: {
       quantity: 0,
-      value: 1
+      value: 1,
     },
     Quarter: {
       quantity: 0,
-      value: 0.25
+      value: 0.25,
     },
     TwoDollars: {
       quantity: 0,
-      value: 2
-    }
-  }
+      value: 2,
+    },
+  },
 };
 
-describe("CoinBank tests", () => {
+describe('CoinBank tests', () => {
   let bank;
 
   beforeEach(() => {
     bank = new CoinBank(DENOMINATIONS);
   });
-  describe("Successful CoinBank Transactions", () => {
-    test("Create an empty bank", () => {
+  describe('Successful CoinBank Transactions', () => {
+    test('Create an empty bank', () => {
       expect(bank).toEqual(DEFAULT_BANK);
     });
-    test("Is coin in inventory for coin that exists", () => {
+    test('Is coin in inventory for coin that exists', () => {
       expect(bank.isCoinInBank(TWO_DOLLAR_ACCESSOR)).toEqual(true);
     });
-    test("Get coin balance with funds available", () => {
+    test('Get coin balance with funds available', () => {
       expect(bank.depositCoin(TWO_DOLLAR_ACCESSOR, 4)).toEqual(true);
       expect(bank.getCoinBalance(TWO_DOLLAR_ACCESSOR)).toEqual(8.0);
     });
-    test("Get number of coins with funds available", () => {
+    test('Get number of coins with funds available', () => {
       expect(bank.depositCoin(TWO_DOLLAR_ACCESSOR, 4)).toEqual(true);
       expect(bank.getCoinNumber(TWO_DOLLAR_ACCESSOR)).toEqual(4);
     });
-    test("Get bank balance with funds available", () => {
+    test('Get bank balance with funds available', () => {
       expect(bank.depositCoin(QUARTER_ACCESSOR, 4)).toEqual(true);
       expect(bank.depositCoin(ONE_DOLLAR_ACCESSOR, 4)).toEqual(true);
       expect(bank.getBalance()).toEqual(5);
     });
-    test("Deposit 5 two dollar coins", () => {
+    test('Deposit 5 two dollar coins', () => {
       expect(bank.depositCoin(TWO_DOLLAR_ACCESSOR, 5)).toEqual(true);
     });
-    test("Deposit 5 dollar coins", () => {
+    test('Deposit 5 dollar coins', () => {
       expect(bank.depositCoin(ONE_DOLLAR_ACCESSOR, 5)).toEqual(true);
     });
-    test("Deposit 5 quarters", () => {
+    test('Deposit 5 quarters', () => {
       expect(bank.depositCoin(QUARTER_ACCESSOR, 5)).toEqual(true);
     });
-    test("Deposit 5 dimes", () => {
+    test('Deposit 5 dimes', () => {
       expect(bank.depositCoin(DIME_ACCESSOR, 5)).toEqual(true);
     });
-    test("Deposit 5 nickels", () => {
+    test('Deposit 5 nickels', () => {
       expect(bank.depositCoin(NICKEL_ACCESSOR, 5)).toEqual(true);
     });
-    test("Withdraw 5 two dollar coins", () => {
+    test('Withdraw 5 two dollar coins', () => {
       expect(bank.depositCoin(TWO_DOLLAR_ACCESSOR, 6)).toEqual(true);
       expect(bank.withdrawCoin(TWO_DOLLAR_ACCESSOR, 5)).toEqual(true);
       expect(bank.getCoinNumber(TWO_DOLLAR_ACCESSOR)).toEqual(1);
     });
-    test("Withdraw 5 dollar coins", () => {
+    test('Withdraw 5 dollar coins', () => {
       expect(bank.depositCoin(ONE_DOLLAR_ACCESSOR, 6)).toEqual(true);
       expect(bank.withdrawCoin(ONE_DOLLAR_ACCESSOR, 5)).toEqual(true);
       expect(bank.getCoinNumber(ONE_DOLLAR_ACCESSOR)).toEqual(1);
     });
-    test("Withdraw 5 quarters", () => {
+    test('Withdraw 5 quarters', () => {
       expect(bank.depositCoin(QUARTER_ACCESSOR, 6)).toEqual(true);
       expect(bank.withdrawCoin(QUARTER_ACCESSOR, 5)).toEqual(true);
       expect(bank.getCoinNumber(QUARTER_ACCESSOR)).toEqual(1);
     });
-    test("Withdraw 5 dimes", () => {
+    test('Withdraw 5 dimes', () => {
       expect(bank.depositCoin(DIME_ACCESSOR, 6)).toEqual(true);
       expect(bank.withdrawCoin(DIME_ACCESSOR, 5)).toEqual(true);
       expect(bank.getCoinNumber(DIME_ACCESSOR)).toEqual(1);
     });
-    test("Withdraw 5 nickels", () => {
+    test('Withdraw 5 nickels', () => {
       expect(bank.depositCoin(NICKEL_ACCESSOR, 6)).toEqual(true);
       expect(bank.withdrawCoin(NICKEL_ACCESSOR, 5)).toEqual(true);
       expect(bank.getCoinNumber(NICKEL_ACCESSOR)).toEqual(1);
     });
   });
 
-  describe("Edge Case CoinBank Transactions", () => {
-    test("Get bank balance with no funds available", () => {
+  describe('Edge Case CoinBank Transactions', () => {
+    test('Get bank balance with no funds available', () => {
       expect(bank.getBalance()).toEqual(0);
     });
-    test("Is coin in inventory for coin that does not exist", () => {
-      expect(bank.isCoinInBank("NotACoin")).toEqual(false);
+    test('Is coin in inventory for coin that does not exist', () => {
+      expect(bank.isCoinInBank('NotACoin')).toEqual(false);
     });
     test("Deposit a coin denomination that doesn't exist in the CoinBank", () => {
-      expect(bank.depositCoin("NotACoin", 5)).toEqual(false);
+      expect(bank.depositCoin('NotACoin', 5)).toEqual(false);
     });
-    test("Deposit a negative amount", () => {
+    test('Deposit a negative amount', () => {
       expect(bank.depositCoin(TWO_DOLLAR_ACCESSOR, -5)).toEqual(false);
     });
-    test("Deposit two dollar coins with no argument", () => {
+    test('Deposit two dollar coins with no argument', () => {
       expect(bank.depositCoin(TWO_DOLLAR_ACCESSOR)).toEqual(false);
     });
-    test("Get coin balance with no funds available", () => {
+    test('Get coin balance with no funds available', () => {
       expect(bank.getCoinBalance(TWO_DOLLAR_ACCESSOR)).toEqual(0);
     });
-    test("Get coin balance for coin that does not exist", () => {
-      expect(bank.getCoinBalance("NotACoin")).toEqual(false);
+    test('Get coin balance for coin that does not exist', () => {
+      expect(bank.getCoinBalance('NotACoin')).toEqual(false);
     });
     test("Withdraw a coin denomination that doesn't exist in the CoinBank", () => {
-      expect(bank.withdrawCoin("NotACoin", 5)).toEqual(false);
+      expect(bank.withdrawCoin('NotACoin', 5)).toEqual(false);
     });
-    test("Withdraw 5 two dollar coins with no funds available", () => {
+    test('Withdraw 5 two dollar coins with no funds available', () => {
       expect(bank.withdrawCoin(TWO_DOLLAR_ACCESSOR, 5)).toEqual(false);
     });
-    test("Withdraw 5 dollar coins with no funds available", () => {
+    test('Withdraw 5 dollar coins with no funds available', () => {
       expect(bank.withdrawCoin(ONE_DOLLAR_ACCESSOR, 5)).toEqual(false);
     });
-    test("Withdraw 5 quarters with no funds available", () => {
+    test('Withdraw 5 quarters with no funds available', () => {
       expect(bank.withdrawCoin(QUARTER_ACCESSOR, 5)).toEqual(false);
     });
-    test("Withdraw 5 dimes with no funds available", () => {
+    test('Withdraw 5 dimes with no funds available', () => {
       expect(bank.withdrawCoin(DIME_ACCESSOR, 5)).toEqual(false);
     });
-    test("Withdraw 5 nickels with no funds available", () => {
+    test('Withdraw 5 nickels with no funds available', () => {
       expect(bank.withdrawCoin(NICKEL_ACCESSOR, 5)).toEqual(false);
     });
   });
