d9abf17fecdf73b03708959cb2cb2bc8fe4ee513
Lindsey <lindsey@users-MBP.hitronhub.home>
2018-02-24 19:00:39 -0800

    decided to refactor inventory into an additional class

---
+++ __mock__/Inventory/inventoryWithTwoRowsData.js
@@ -0,0 +1,29 @@
+module.exports = {
+  inventory: {
+    A: {
+      row: {
+        A: [
+          {
+            item: { name: 'Pepsi', price: 2.5 },
+            quantity: 0,
+          },
+          { item: { name: 'Coke', price: 1.45 }, quantity: 0 },
+        ],
+      },
+      rowName: 'A',
+    },
+    B: {
+      row: {
+        B: [
+          {
+            item: { name: 'Pixie Stix', price: 0.5 },
+            quantity: 0,
+          },
+          { item: { name: 'Dr. Pepper', price: 2.45 }, quantity: 0 },
+        ],
+      },
+      rowName: 'B',
+    },
+  },
+  maxRowSize: 100,
+};
--- __tests__/Inventory.spec.js
+++ __tests__/Inventory.spec.js
@@ -1,208 +1,101 @@
 const Inventory = require('../src/Inventory');
 const Item = require('../src/Item');
-
-const unchangedInventoryWithItems = {
-  A2: { item: { name: 'Coke', price: 1.45 }, quantity: 0 },
-  A1: {
-    item: {
-      name: 'Pepsi',
-      price: 2.5,
-    },
-    quantity: 0,
-  },
-};
+const Row = require('../src/Row');
+const inventoryWithTwoRowsData = require('../__mock__/Inventory/inventoryWithTwoRowsData');
+const inventoryWithThreeRowsData = require('../__mock__/Inventory/inventoryWithThreeRowsData');
+// A: {[{item, quantity}, {item,quantity}, {item, quantity}]}
+// B: {[{item, quantity}, {item,quantity}, {item, quantity}]}
 
 describe('Iventory tests', () => {
   let inventory;
   let inventoryWithItems;
+  let rowA;
+  let rowB;
+  let rowC;
   let item1;
   let item2;
   let item3;
   let item4;
   beforeEach(() => {
-    inventory = new Inventory();
     item1 = new Item('Pepsi', 2.5);
     item2 = new Item('Coke', 1.45);
     item3 = new Item('Pixie Stix', 0.5);
     item4 = new Item('Dr. Pepper', 2.45);
-    inventoryWithItems = new Inventory([{ A1: item1 }, { A2: item2 }]);
+    rowA = new Row('A', [item1, item2]);
+    rowB = new Row('B', [item3, item4]);
+    rowC = new Row('C', [item1, item3]);
+    inventory = new Inventory();
+    inventoryWithItems = new Inventory([rowA, rowB]);
   });
   describe('Successful Inventory Transactions', () => {
-    test('Create an inventory with 0 items', () => {
-      expect(inventory).toEqual({ inventory: {} });
+    test('Create an inventory with 2 items', () => {
+      expect(inventoryWithItems).toEqual(inventoryWithTwoRowsData);
     });
-    test('Create inventory with 4 items', () => {
-      const inventory1 = new Inventory([
-        { A1: item1 },
-        { A2: item2 },
-        { A3: item3 },
-        { A4: item4 },
-      ]);
-      expect(inventory1).toEqual({
-        inventory: {
-          A1: { item: { name: 'Pepsi', price: 2.5 }, quantity: 0 },
-          A2: {
-            item: { name: 'Coke', price: 1.45 },
-            quantity: 0,
-          },
-          A3: {
-            item: {
-              name: 'Pixie Stix',
-              price: 0.5,
-            },
-            quantity: 0,
-          },
-          A4: { item: { name: 'Dr. Pepper', price: 2.45 }, quantity: 0 },
-        },
-      });
+    test('Add a row to inventory with 2 items', () => {
+      expect(inventoryWithItems.addRow(rowC)).toEqual(true);
+      expect(inventoryWithItems.getInventory()).toEqual(inventoryWithThreeRowsData);
     });
-    test('Add an item to inventory with quantity not defined', () => {
-      inventory.addEntry('A1', item1);
-      expect(inventory.getInventory()).toEqual({
-        A1: { name: 'Pepsi', price: 2.5 },
-        quantity: 0,
-      });
+    test('Update price of a specific item in inventory', () => {
+      expect(inventoryWithItems.updatePrice('A', 0, 3.0)).toEqual(true);
     });
-    test('Add an item to inventory with quantity defined', () => {
-      expect(inventory.addEntry('A1', item1, 5)).toEqual(true);
-      expect(inventory.getInventory()).toEqual({
-        A1: { name: 'Pepsi', price: 2.5 },
-        quantity: 5,
-      });
+    test('Get a specific item in inventory', () => {
+      expect(inventoryWithItems.getItem('A', 0)).toEqual({ name: 'Pepsi', price: 2.5 });
     });
-    test('Remove an item from inventory', () => {
-      expect(inventoryWithItems.removeEntry('A2')).toEqual(true);
-      expect(inventoryWithItems.getInventory()).toEqual({
-        A1: {
-          item: {
-            name: 'Pepsi',
-            price: 2.5,
-          },
-          quantity: 0,
-        },
-      });
+    test('Get a specific item quantity in inventory', () => {
+      expect(inventoryWithItems.getItemQuantity('A', 0)).toEqual(0);
     });
-    test('Adjust the price of an item in inventory', () => {
-      expect(inventoryWithItems.updatePrice('A1', 1.05)).toEqual(true);
-      expect(inventoryWithItems.getInventory()).toEqual({
-        A2: { item: { name: 'Coke', price: 1.45 }, quantity: 0 },
-        A1: {
-          item: {
-            name: 'Pepsi',
-            price: 1.05,
-          },
-          quantity: 0,
-        },
-      });
+    test('Remove a row from inventory', () => {
+      expect(inventoryWithItems.addRow(rowC)).toEqual(true);
+      expect(inventoryWithItems.getInventory()).toEqual(inventoryWithThreeRowsData);
+      expect(inventoryWithItems.removeRow('C')).toEqual(true);
     });
-    test('Adjust the name of an item in inventory', () => {
-      expect(inventoryWithItems.updateName('A1', 'A3')).toEqual(true);
-      expect(inventoryWithItems.getInventory()).toEqual({
-        A2: { item: { name: 'Coke', price: 1.45 }, quantity: 0 },
-        A3: {
-          item: {
-            name: 'Pepsi',
-            price: 2.5,
-          },
-          quantity: 0,
-        },
-      });
+    test('Increase an item in inventory', () => {
+      expect(inventoryWithItems.increaseQuantity('A', 0, 3)).toEqual(true);
     });
-    test('Increase the quantity of an item in inventory', () => {
-      expect(inventoryWithItems.increaseQuantity('A1', 3)).toEqual(true);
-      expect(inventoryWithItems.getInventory()).toEqual({
-        A2: { item: { name: 'Coke', price: 1.45 }, quantity: 0 },
-        A1: {
-          item: {
-            name: 'Pepsi',
-            price: 2.5,
-          },
-          quantity: 3,
-        },
-      });
-    });
-    test('Get a listing of every item in inventory', () => {
-      expect(inventoryWithItems.getInventory()).toEqual({
-        A2: { item: { name: 'Coke', price: 1.45 }, quantity: 0 },
-        A1: {
-          item: {
-            name: 'Pepsi',
-            price: 2.5,
-          },
-          quantity: 0,
-        },
-      });
-    });
-    test('Get a particular item in inventory', () => {
-      const item = inventoryWithItems.getItem('A1');
-      expect(item).toEqual(item1);
-    });
-    test('Test if an item with quantity 0 in inventory is empty', () => {
-      expect(inventoryWithItems.isEmpty('A1')).toEqual(true);
-    });
-    test('Test if an item with quantity not 0 in inventory is empty', () => {
-      expect(inventoryWithItems.increaseQuantity('A1', 3)).toEqual(true);
-      expect(inventoryWithItems.isEmpty('A1')).toEqual(false);
-    });
-    test('Decrease the quantity of an item in inventory', () => {
-      expect(inventoryWithItems.increaseQuantity('A1', 3)).toEqual(true);
-      expect(inventoryWithItems.decreaseQuantity('A1', 2)).toEqual(true);
-      expect(inventoryWithItems.getInventory()).toEqual({
-        A2: { item: { name: 'Coke', price: 1.45 }, quantity: 0 },
-        A1: {
-          item: {
-            name: 'Pepsi',
-            price: 2.5,
-          },
-          quantity: 1,
-        },
-      });
+    test('Decrease an item in inventory', () => {
+      expect(inventoryWithItems.increaseQuantity('A', 0, 3)).toEqual(true);
+      expect(inventoryWithItems.decreaseQuantity('A', 0, 2)).toEqual(true);
+      expect(inventoryWithItems.getItemQuantity('A', 0)).toEqual(1);
     });
   });
   describe('Edge Case Inventory Transactions', () => {
-    test('Add an item to inventory that already exists', () => {
-      expect(inventoryWithItems.addEntry('A1', item1)).toEqual(false);
-      expect(inventoryWithItems.getInventory()).toEqual(unchangedInventoryWithItems);
+    test('Create an inventory with 0 items', () => {
+      expect(inventory).toEqual({ inventory: {}, maxRowSize: 100 });
     });
-    test('Add an item to inventory with an invalid entry name', () => {
-      expect(inventoryWithItems.addEntry('', item1)).toEqual(false);
-      expect(inventoryWithItems.getInventory()).toEqual(unchangedInventoryWithItems);
+    test('Add a row to inventory with 0 items', () => {
+      expect(inventory.addRow(rowC)).toEqual(true);
     });
-    test('Remove an item that does not exist from inventory', () => {
-      expect(inventory.removeEntry('A1')).toEqual(false);
-      expect(inventory.getInventory()).toEqual({});
+    test('Add a row that already exists', () => {
+      expect(inventoryWithItems.addRow(rowA)).toEqual(false);
     });
-    test('Update the price of an item that does not exist in inventory', () => {
-      expect(inventoryWithItems.updatePrice('A7', 2.59)).toEqual(false);
-      expect(inventoryWithItems.getInventory()).toEqual(unchangedInventoryWithItems);
+    test('Get an item in inventory with an invalid itemIndex', () => {
+      expect(inventoryWithItems.getItem('A', -1)).toEqual({});
     });
-    test('Update an item name that does not exist in inventory', () => {
-      expect(inventoryWithItems.updateName('A8')).toEqual(false);
-      expect(inventoryWithItems.getInventory()).toEqual(unchangedInventoryWithItems);
+    test('Get a specific item quantity with an invalid itemIndex', () => {
+      expect(inventoryWithItems.getItemQuantity('A', 'a')).toEqual(-1);
     });
-    test('Decrease an item quantity with a negative quantity', () => {
-      expect(inventoryWithItems.decreaseQuantity('A1', -6)).toEqual(false);
-      expect(inventoryWithItems.getInventory()).toEqual(unchangedInventoryWithItems);
+    test('Remove a row from inventory with 0 items', () => {
+      expect(inventory.removeRow('C')).toEqual(false);
     });
-    test('Decrease an item quantity with a float quantity', () => {
-      expect(inventoryWithItems.decreaseQuantity('A1', 6.5)).toEqual(false);
-      expect(inventoryWithItems.getInventory()).toEqual(unchangedInventoryWithItems);
+    test('Remove a non-existent row from inventory', () => {
+      expect(inventoryWithItems.removeRow('D')).toEqual(false);
     });
-    test('Decrease an item quantity with quantity of 0 in inventory', () => {
-      expect(inventoryWithItems.decreaseQuantity('A1', 6)).toEqual(false);
-      expect(inventoryWithItems.getInventory()).toEqual(unchangedInventoryWithItems);
+    test('Update price of a non-existent item in inventory', () => {
+      expect(inventoryWithItems.updatePrice('D', 0, 3.0)).toEqual(false);
     });
-    test("Decrease an item quantity for an item that doesn't exist in inventory", () => {
-      expect(inventoryWithItems.decreaseQuantity('A7', 2)).toEqual(false);
-      expect(inventoryWithItems.getInventory()).toEqual(unchangedInventoryWithItems);
+    test('Increase a non-existent item in inventory', () => {
+      expect(inventoryWithItems.increaseQuantity('D', 0, 3.0)).toEqual(false);
     });
-    test("Increase an item quantity for an item that doesn't exist in inventory", () => {
-      expect(inventoryWithItems.increaseQuantity('A7', 2)).toEqual(false);
-      expect(inventoryWithItems.getInventory()).toEqual(unchangedInventoryWithItems);
+    test('Increase an item with a negative number in inventory', () => {
+      expect(inventoryWithItems.increaseQuantity('A', 0, -3)).toEqual(false);
     });
-    test('Adjust the price of an item in inventory to -1.05', () => {
-      expect(inventoryWithItems.updatePrice('A1', -1.05)).toEqual(false);
-      expect(inventoryWithItems.getInventory()).toEqual(unchangedInventoryWithItems);
+    test('Decrease an item with a negative number in inventory', () => {
+      expect(inventoryWithItems.decreaseQuantity('A', 0, -3)).toEqual(false);
+    });
+    test('Decrease an item in inventory beyond quantity available', () => {
+      expect(inventoryWithItems.increaseQuantity('A', 0, 3)).toEqual(true);
+      expect(inventoryWithItems.decreaseQuantity('A', 0, 4)).toEqual(false);
+      expect(inventoryWithItems.getItemQuantity('A', 0)).toEqual(3);
     });
   });
 });
